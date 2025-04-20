4e3592ebadae2c14acda6b94b56c7c731741cdec
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-04-20 19:55:47 +0100

    feat: integrated token listener and query client in layout
    - Added useTokenListener to check token every minute
    - Wrapped layout in QueryClientProvider for React Query support

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,9 @@ import { Slot } from "expo-router";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import AuthProvider from "./(providers)/auth.provider";
+import { useTokenListener } from "../hooks/token.hook";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
@@ -12,7 +14,9 @@ export { ErrorBoundary } from "expo-router";
 export const unstable_settings = { ignorePatterns: ["_*"] };
 
 preventAutoHideAsync();
+const client = new QueryClient();
 export default function () {
+  useTokenListener(60000); // Check token every minute
   const [loaded, error] = useFonts({ ...FontAwesome.font });
 
   React.useEffect(() => {
@@ -24,11 +28,13 @@ export default function () {
   }, [loaded]);
 
   return (
-    <GestureHandlerRootView className="flex-1">
-      <AuthProvider>
-        <Slot />
-        <StatusBar animated translucent networkActivityIndicatorVisible />
-      </AuthProvider>
-    </GestureHandlerRootView>
+    <QueryClientProvider client={client}>
+      <GestureHandlerRootView className="flex-1">
+        <AuthProvider>
+          <Slot />
+          <StatusBar animated translucent networkActivityIndicatorVisible />
+        </AuthProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
 }
