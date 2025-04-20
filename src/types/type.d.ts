cc825de8d0a5d12045e658959ba59dd0c1d60558
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-04-20 19:54:22 +0100

    feat: added IWallet interface to type definitions
    - Defined IWallet interface with properties: id, amount, updated_at, created_at, total_deposit, total_withdrawn

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -6,3 +6,12 @@ interface IUser {
     last_name: string;
     first_name: string;
 };
+
+interface IWallet {
+    id: number;
+    amount: string;
+    updated_at: string;
+    created_at: string;
+    total_deposit: string;
+    total_withdrawn: string;
+}
