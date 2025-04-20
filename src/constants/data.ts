d085d9d61a62a974d8eefe4e64b67ff7db3de029
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-04-20 19:54:57 +0100

    feat: updated onboarding slides with local images
    - Replaced imageUri with local image imports for onboarding slides
    - Ensured consistent image usage across all slides

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -2,16 +2,16 @@ export const slides = [
     {
         title: 'Schedule Your Rides',
         description: 'Drivers can easily schedule rides ahead of time with specified routes and times.',
-        imageUri: 'https://raw.githubusercontent.com/feathericons/feather/master/icons/calendar.svg',
+        image: require("../assets/images/onboarding/slide1.png"),
     },
     {
         title: 'Find Available Rides',
         description: 'Browse scheduled rides from drivers and book the ones that match your needs.',
-        imageUri: 'https://raw.githubusercontent.com/feathericons/feather/master/icons/search.svg',
+        image: require("../assets/images/onboarding/slide2.png"),
     },
     {
         title: 'Secure Payments',
         description: 'Pay for your rides securely within the app using multiple payment methods.',
-        imageUri: 'https://raw.githubusercontent.com/feathericons/feather/master/icons/credit-card.svg',
+        image: require("../assets/images/onboarding/slide3.png"),
     },
 ];
