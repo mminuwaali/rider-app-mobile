53352bb3e39f05d7a7164abc272c8967b1551424
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-04-22 12:33:07 +0100

    fix: corrected import path for slides and refactored component structure - Updated import path for slides to maintain consistency - Refactored component structure for improved readability - Added back button handler for Android devices

--- src/app/(onboard)/index.tsx
+++ src/app/(onboard)/index.tsx
@@ -1,51 +1,81 @@
 import React from "react";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
-import { slides } from "@/src/constants/data";
+import { slides } from "@/constants/data";
 import Animated from "react-native-reanimated";
 import { Slider } from "./_sections.tsx/slider";
-import { Text, TouchableOpacity, View } from "react-native";
+import {
+  Text,
+  View,
+  Platform,
+  BackHandler,
+  TouchableOpacity,
+} from "react-native";
 
 export default function () {
-    const [index, setIndex] = React.useState(0);
-    const swiperRef = React.useRef<Swiper>(null);
+  const [index, setIndex] = React.useState(0);
+  const swiperRef = React.useRef<Swiper>(null);
 
-    const handleSkip = () => {
-        router.replace('/sign-in');
-    };
+  React.useEffect(() => {
+    function closeAppOnBackButton() {
+      if (Platform.OS === "android") {
+        BackHandler.exitApp();
+        return true;
+      }
+      return false;
+    }
 
-    const handleNext = () => {
-        if (index + 1 < slides.length) return setIndex(index + 1);
-        handleSkip();
-    };
-
-    return (
-        <Animated.View className="flex-1 gap-4 bg-white">
-            <TouchableOpacity onPress={handleSkip} className='absolute top-12 right-6 z-10'>
-                <Text className='text-accent text-base font-medium'>Skip</Text>
-            </TouchableOpacity>
-
-            <Swiper
-                loop={false}
-                index={index}
-                ref={swiperRef}
-                paginationStyle={{ bottom: 20 }}
-                onIndexChanged={(index) => setIndex(index)}
-                dot={<View className='bg-gray-300 w-2 h-2 rounded-full mx-1' />}
-                activeDot={<View className='bg-slate-700 w-3 h-3 rounded-full mx-1' />}
-            >
-                {slides.map((slide, index) => (
-                    <Slider {...slide} key={index} />
-                ))}
-            </Swiper>
-
-            <View className="px-10% pb-10">
-                <TouchableOpacity onPress={handleNext} className='bg-cyan-600 py-4 rounded-lg items-center'>
-                    <Text className='text-white font-semibold text-lg'>
-                        {index === slides.length - 1 ? 'Get Started' : 'Next'}
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        </Animated.View>
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      closeAppOnBackButton
     );
+
+    // Clean up the event listener when component unmounts
+    return () => backHandler.remove();
+  }, []);
+
+  const handleSkip = () => {
+    router.replace("/sign-in");
+  };
+
+  const handleNext = () => {
+    if (index + 1 < slides.length) return setIndex(index + 1);
+    handleSkip();
+  };
+
+  return (
+    <Animated.View className="flex-1 gap-4 bg-white">
+      <TouchableOpacity
+        onPress={handleSkip}
+        className="absolute top-12 right-6 z-10"
+      >
+        <Text className="text-accent text-base font-medium">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        loop={false}
+        index={index}
+        ref={swiperRef}
+        paginationStyle={{ bottom: 20 }}
+        onIndexChanged={(index) => setIndex(index)}
+        dot={<View className="bg-gray-300 w-2 h-2 rounded-full mx-1" />}
+        activeDot={<View className="bg-slate-700 w-3 h-3 rounded-full mx-1" />}
+      >
+        {slides.map((slide, index) => (
+          <Slider {...slide} key={index} />
+        ))}
+      </Swiper>
+
+      <View className="px-10% pb-10">
+        <TouchableOpacity
+          onPress={handleNext}
+          className="bg-cyan-600 py-4 rounded-lg items-center"
+        >
+          <Text className="text-white font-semibold text-lg">
+            {index === slides.length - 1 ? "Get Started" : "Next"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </Animated.View>
+  );
 }
